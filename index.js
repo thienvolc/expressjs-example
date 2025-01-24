d67e62b4f48d1c3610495a423daf1d2bd49aee0a
= <=>
2025-01-24 17:44:32 +0700

    fix index.js for auto connection for environment variables

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 import app from './src/app.js';
 import Server from './src/server/server.js';
-import { AppConfig, DBConfig, isDevEnvironment } from './src/configs/index.js';
-console.log("🚀 ~ AppConfig:", AppConfig)
-import { createDBConnectionByType } from './src/dbs/index.js';
+import { AppConfig } from './src/configs/index.js';
 
-const connection = createDBConnectionByType(DBConfig.type);
-connection.setConfig(DBConfig);
-if (isDevEnvironment(AppConfig.environment)) {
-    connection.setDebug();
-    connection.setSafePoolSize();
-}
-await connection.connect();
+import { createDBConnection } from './src/dbs/index.js';
+
+const DBconnection = createDBConnection();
+await DBconnection.connect();
 
 const serverConfig = { ...AppConfig, app };
 const server = Server.createByConfig(serverConfig);
--- src/dbs/index.js
+++ src/dbs/index.js
@@ -1,12 +1,27 @@
-import { DBType } from '../configs/index.js';
+import { AppConfig, DBConfig, DBType, isDevEnvironment } from '../configs/index.js';
 import { MongoConnectionSingletonFactory } from './mongo/mongo-connection.js';
+
 // import { MySQLConnectionSingletonFactory } from './mysql/mysql-connection.js';
 
-export const createDBConnectionByType = (dbType) => {
+export const createDBConnection = () => {
+    const DBconnection = createDBConnectionByType(DBConfig.type);
+    setupForDevelopment(DBconnection);
+    return DBconnection;
+};
+
+const createDBConnectionByType = (dbType) => {
     switch (dbType) {
         case DBType.MONGO:
             return MongoConnectionSingletonFactory.createConnection();
         default:
             throw new Error('Database not supported');
+    }
+};
+
+export const setupForDevelopment = (DBconnection) => {
+    DBconnection.setConfig(DBConfig);
+    if (isDevEnvironment(AppConfig.environment)) {
+        DBconnection.setDebug();
+        DBconnection.setSafePoolSize();
     }
 };
